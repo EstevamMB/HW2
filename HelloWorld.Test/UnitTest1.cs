7aaf551695e1a7cb8bf1b47cc5c5a98e702dcf98
EstevamMB <estevamaugusto@gmail.com>
2023-05-20 11:51:34 -0300

    inicio projeto setup c#

---
+++ HelloWorld.Test/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace HelloWorld.Test;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
